4fe32643dfb2221156178c88887143cb3cdf39d0
dytq <taariq.deda@gmail.com>
2022-02-25 23:43:23 +0100

    ajout algo comptage point chevalier

--- src/Chevalier.cpp
+++ src/Chevalier.cpp
@@ -11,7 +11,48 @@ Chevalier::Chevalier(Joueur * joueur, Noeud * noeud)
 Chevalier::~Chevalier()
 {}
 
+/**
+ * Algorithme du comptage des points pour le Chevalier.
+ *
+ * Parcours du réseau de noeuds en profondeur.
+ **/
 bool Chevalier::compter_points(int status_du_jeu, std::map<Joueur *, std::list<Meeple *>> mapJoueurListeMeeple, int * score)
 {
-    return false;
+    std::list<Noeud*> pileNoeud;   // pile pour le parcours des fils
+    std::list<Noeud*> noeudMarque; // marque tous les noeuds rencontrés
+
+    bool isComplete = true;
+
+    *score = this->noeud->get_points(status_du_jeu);
+    pileNoeud.push_back(this->noeud);
+    noeudMarque.push_back(this->noeud);
+
+    while(!pileNoeud.empty())
+    {
+        std::list<Noeud*>::iterator iterNoeud = pileNoeud.begin();
+
+        Noeud * noeudCentrale = *iterNoeud;
+        pileNoeud.pop_front();
+
+        int i;
+
+        for(i = 0; i < noeudCentrale->get_nbr_voisins(); i++)
+        {
+            Noeud * noeud_fils = noeudCentrale->noeud_fils(nullptr, i);
+
+            if(noeudCentrale->has_nullptr())
+            {
+                isComplete = false;
+            }
+
+            if(std::find(noeudMarque.begin(), noeudMarque.end(), noeud_fils) == noeudMarque.end())
+            {
+                noeudMarque.push_back(noeud_fils);
+                pileNoeud.push_back(noeud_fils);
+                *score = noeud_fils->get_points(status_du_jeu);
+            }
+        }
+    }
+
+    return isComplete;
 }
