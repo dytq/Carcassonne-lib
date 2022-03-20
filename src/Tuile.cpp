6ad5f7b312da3138087c215a87633c77f54a5434
HaitoWolf <91672479+HaitoWolf@users.noreply.github.com>
2022-03-20 14:35:37 +0100

    Update Tuile.cpp
    Suppression tuilesVoisines

--- src/Tuile.cpp
+++ src/Tuile.cpp
@@ -2,23 +2,9 @@
 #include "Tuile.hpp"
 
 // FONCTIONS
-Tuile::Tuile(std::array<Tuile *, 4> tuilesVoisines, std::array<Bordure *,4> bordure, std::list<Element *> element)
-{
-    this->tuilesVoisines = tuilesVoisines;
-    this->bordure = bordure;
-    this->element = element;
-}
-
 Tuile::Tuile(std::array<Bordure *,4> bordure, std::list<Element *> element)
 {
-    std::array<Tuile *, 4> tmp;
-
-    for(int i = 0; i < 4; i++)
-    {
-        tmp[i] = NULL;
-    }
-
-    this->tuilesVoisines = tmp;
+    this->tuilesVoisines = tuilesVoisines;
     this->bordure = bordure;
     this->element = element;
 }
